cc213a1370218f4795fdcf8a783a80d6b96d9545
Simon Reinhardt <simon.reinhardt@stud.uni-regensburg.de>
2015-10-08 10:25:47 +0200

    Update README and configure

--- src/program/debug.c
+++ src/program/debug.c
@@ -247,7 +247,7 @@ static void check_regex (void)
   exit (0);
 }
 
-static error_t _GL_ATTRIBUTE_PURE
+static error_t
 parse_opt (int key, _GL_UNUSED char *arg, _GL_UNUSED struct argp_state *state)
 {
   switch (key)
