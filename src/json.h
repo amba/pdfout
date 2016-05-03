87d82c9a60fca10eecfa64c871a2a1dcf1a200b4
Simon Reinhardt <simon.reinhardt@stud.uni-regensburg.de>
2016-05-03 18:02:38 +0200

    Add JSON Emitter.

--- src/json.h
+++ src/json.h
@@ -16,18 +16,27 @@ typedef enum json_token_e {
   JSON_BEGIN_OBJECT, 
   JSON_END_OBJECT,
 
+  /* Emitter events.  */
+  JSON_BEGIN_BLOCK_ARRAY = 128,
+  JSON_BEGIN_BLOCK_OBJECT,
 } json_token;
 
 
 typedef struct json_parser_s json_parser;
+typedef struct json_emitter_s json_emitter;
 
 void json_parser_drop (fz_context *ctx, json_parser *parser);
+void json_emitter_drop (fz_context *ctx, json_emitter *emitter);
 
 json_parser *json_parser_new (fz_context *ctx, fz_stream *stm);
+json_emitter *json_emitter_new (fz_context *ctx, fz_output *stm);
 
-
+/* The following two functions do not throw on invalid input. Otherwise, it
+   could make the valgrind tests slower.  */
 json_token json_parser_parse (fz_context *ctx, json_parser *parser,
 			      char **value, int *value_len);
-
+/* Return non-zero on failure.  */
+int json_emitter_emit (fz_context *ctx, json_emitter *emitter,
+		       json_token event, const char *value, int value_len);
 
 #endif	/* PDFOUT_HAVE_JSON_H */
